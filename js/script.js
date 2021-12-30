527484f8525ce2011f825d97100aab115d6783cc
sarah_d <sarah.d.garcia@maricopa.gov>
2021-12-30 10:34:22 -0700

    added functions to fetch and display repos

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ const overview = document.querySelector(".overview");
 //my github username 
 const username = "sarah-d-garcia";
 
+//Select unordered list to display the repos
+const repoList =  document.querySelector(".repo-list");
+
 
 
 //-----async function to get my data-----------------------------
@@ -16,11 +19,6 @@ const gitUserInfo = async function () {
   };
 
   gitUserInfo();
-
-
-//-----Functions to run the page
-
-
 
 //Funciton to display the data fected
 const displayUserInfo = function (data) {
@@ -38,5 +36,22 @@ const displayUserInfo = function (data) {
     </div>`;
 
   overview.append(div);  
+  gitUserRepos();
 };
 
+//-----Async function to get my repos--------------------------------
+const gitUserRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+//Function to display the repos
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      repoList.append(repoItem);
+    }
+  };
